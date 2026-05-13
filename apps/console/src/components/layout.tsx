50910f1044d52cd62803fcc41766b99daa5e22e6
Casprine Assempah <casprine.001@gmail.com>
2026-05-13 17:32:42 +0200

    chore(console): add oxc tooling
    -add oxlint and oxfmt scripts;
    
    -lock console Oxc dependencies;

--- apps/console/src/components/layout.tsx
+++ apps/console/src/components/layout.tsx
@@ -17,7 +17,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen max-w-4xl mx-auto py-10">
       <Header />
-      {/* {children} */}
+      {children}
     </div>
   );
 };
